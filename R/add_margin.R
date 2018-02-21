71006448528708430801f02e0c0d0db89d11f1a3
Thevaa Chandereng <chandereng@wisc.edu>
2018-02-21 01:30:58 -0600

    added R file

--- R/add_margin.R
+++ R/add_margin.R
@@ -1,7 +1,32 @@
+#' Sample Size Calculation for Non-Inferiority Trial for Additive Mean Hypothesis
+#'
+#' This function computes the required sample size for non-inferiority trial with additive margin.
+#' It also computes the sample size required for both treatment and control group.
+#' It also computes the efficiency compared to equal treatment allocation.
+#'
+#' @param delta a numerical value of the additive value in the hypothesis ($\Delta > 0$)
+#' @param varC a numerical value of the variance of the control group
+#' @param varT a numerical value of the variance of the treatment group, it is null if it is same as varC
+#' @param alpha  Two-sided Type-I-error
+#' @param beta  Type-II-error
+#' @return a list of sample sizes for control, treatment, and randomization ratio for treatment to control and efficiency compared 1:1 randomization ratio
+#'
+#'
+#'
+#' @example add.margin(0.04, 20, 18)
+#'
+#'
+#' @author Thevaa Chandereng, Rick Chappell
+#'
+#'
+
+
+
 add.margin <- function(delta, varC, varT = NULL, alpha = 0.05, beta = 0.20){
   stopifnot(delta > 0, alpha  > 0, beta > 0, varC > 0)
   if(is.null(varT)){
     k <- 1
+    varT <- varC
   }
   else{
     k <- sqrt(varC) / sqrt(varT)
--- R/multi_margin.R
+++ R/multi_margin.R
@@ -1,7 +1,31 @@
+#' Sample Size Calculation for Non-Inferiority Trial for Multiplicative Mean Hypothesis
+#'
+#' This function computes the required sample size for non-inferiority trial with multiplicative margin.
+#' It also computes the sample size required for both treatment and control group.
+#' It also computes the efficiency compared to equal treatment allocation.
+#'
+#' @param delta a numerical value of the additive value in the hypothesis ($\Delta > 1$)
+#' @param varC a numerical value of the variance of the control group
+#' @param varT a numerical value of the variance of the treatment group, it is null if it is same as varC
+#' @param alpha  Two-sided Type-I-error
+#' @param beta  Type-II-error
+#' @return a list of sample sizes for control, treatment, and randomization ratio for treatment to control (k:1) and efficiency compared 1:1 randomization ratio
+#'
+#'
+#' @example multi.margin(0.04, 20, 18)
+#'
+#'
+#' @author Thevaa Chandereng, Rick Chappell
+#'
+#'
+
+
+
 multi.margin <- function(delta, varC, varT = NULL, alpha = 0.05, beta = 0.20){
   stopifnot(delta > 1, alpha  > 0, beta > 0, varC > 0)
   if(is.null(varT)){
     k <- 1 / (delta)
+    varT <- varC
   }
   else{
     k <- sqrt(varC) / (delta * sqrt(varT))
